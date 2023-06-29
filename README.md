22abd8e651c95bb082ef2636a1bd7b9a202d5909
alchemy <irainsun@Live.cn>
2023-06-29 22:35:39 +0800

    add readme file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # proto2gin_tool
+
+
+
+-  可以方便的使用格式化的protobuf文件生成gin的接口函数，从而增加了代码的复用性
+-  在使用proto生成代码时需要使用到"google/api/annotations.proto"包
+-  可以通过go install生成可安装的可执行文件来更方便的调用
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/types/pluginpb"
 
-	"GoStart/tools/generator"
+	"proto2gin_tool/generator"
 )
 
 func main() {
